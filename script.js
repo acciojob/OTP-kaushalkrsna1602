5881d5a72ed117f1ef298ab1e371f50d71176304
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-20 19:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,3 +22,73 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+describe('OTP Input Test', () => {
+  it('should fill OTP fields and move focus correctly', () => {
+    cy.visit(baseUrl + "/main.html"); // Make sure the URL is correct
+
+    // Ensure the first input gets focus
+    cy.get('.code').eq(0).focus().should('have.attr', 'id', 'code-1');
+    
+    // Typing the OTP and checking focus shift
+    cy.get('.code').eq(0).type('5'); // Type into the first input
+    cy.focused().should('have.id', 'code-2'); // Ensure focus moves to the next input
+
+    cy.get('.code').eq(1).type('1');
+    cy.focused().should('have.id', 'code-3');
+
+    cy.get('.code').eq(2).type('7');
+    cy.focused().should('have.id', 'code-4');
+
+    cy.get('.code').eq(3).type('2');
+    cy.focused().should('have.id', 'code-5');
+
+    cy.get('.code').eq(4).type('9');
+    cy.focused().should('have.id', 'code-6');
+
+    cy.get('.code').eq(5).type('6');
+  });
+
+  it('should handle backspace correctly', () => {
+    cy.visit(baseUrl + "/main.html");
+
+    // Focus the first input
+    cy.get('.code').eq(0).focus().should('have.attr', 'id', 'code-1');
+
+    // Simulate typing and using backspace
+    cy.get('.code').eq(0).type('5');
+    cy.get('.code').eq(1).type('1');
+    cy.get('.code').eq(2).type('7');
+    cy.get('.code').eq(3).type('2');
+    cy.get('.code').eq(4).type('9');
+    cy.get('.code').eq(5).type('6');
+    
+    // Pressing backspace should focus on the previous input
+    cy.get('.code').eq(5).type('{backspace}'); // Move focus back to code-5
+    cy.focused().should('have.id', 'code-5');
+
+    cy.get('.code').eq(4).type('{backspace}');
+    cy.focused().should('have.id', 'code-4');
+
+    cy.get('.code').eq(3).type('{backspace}');
+    cy.focused().should('have.id', 'code-3');
+
+    cy.get('.code').eq(2).type('{backspace}');
+    cy.focused().should('have.id', 'code-2');
+
+    cy.get('.code').eq(1).type('{backspace}');
+    cy.focused().should('have.id', 'code-1');
+  });
+});
+
