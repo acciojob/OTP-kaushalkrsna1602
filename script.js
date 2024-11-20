334fb1a5a867b82bf51362f355c55a3dba77e3ba
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-20 19:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code")
+
+inputs.forEach((input, index) => {
+	input.addEventListener("input", (e) => {
+		const current = e.target
+		const next = inputs[index + 1]
+		if(current.value && next) 
+		{
+			next.focus()
+		}
+	});
+
+	input.addEventListener('keydown' , (e) => {
+		const current = e.target
+		const prev = inputs[index - 1]
+		if(e.key === 'Backspace' && !current.value && prev) 
+		{
+			prev.focus();
+		}
+	})
+})
