4623e80e0c5df6edd1740907234484ab82e5a0bf
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-20 19:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const inputs = document.querySelectorAll(".code")
+const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
-	input.addEventListener("input", (e) => {
-		const current = e.target
-		const next = inputs[index + 1]
-		if(current.value && next) 
-		{
-			next.focus()
-		}
-	});
+    input.addEventListener("input", (e) => {
+        const current = e.target;
+        const next = inputs[index + 1];
+        
+        if (current.value && next) {
+            next.focus(); 
+        }
+    });
 
-	input.addEventListener('keydown' , (e) => {
-		const current = e.target
-		const prev = inputs[index - 1]
-		if(e.key === 'Backspace' && !current.value && prev) 
-		{
-			prev.focus();
-		}
-	})
-})
+    input.addEventListener("keydown", (e) => {
+        const current = e.target;
+        const prev = inputs[index - 1];
+        
+        
+        if (e.key === "Backspace" && !current.value && prev) {
+            prev.focus();
+        }
+    });
+});
